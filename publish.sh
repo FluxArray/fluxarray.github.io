87eaf8e41100f96894f574f0a949ee0eeeacb0cc
Fluxarray <flux4rray@gmail.com>
2026-07-07 13:47:41 +0530

    meh 2026-07-07

--- publish.sh
+++ publish.sh
@@ -1,5 +1,4 @@
 #!/bin/zsh
-
 OBSIDIAN_POSTS="$HOME/Documents/Obsidian Notes/NOTES/00 Posts"
 OBSIDIAN_IMAGES="$HOME/Documents/Obsidian Notes/NOTES/98 Images"
 
@@ -7,13 +6,15 @@ VITE_CONTENT_DIR="./public/content"
 VITE_IMAGE_DIR="./public/images"
 
 echo "Syncing notes and images from Obsidian..."
-
 rsync -av --delete --exclude="manifest.json" "$OBSIDIAN_POSTS/" "$VITE_CONTENT_DIR/"
 rsync -av --delete "$OBSIDIAN_IMAGES/" "$VITE_IMAGE_DIR/"
 
+echo "Rebuilding Virtual File System Map..."
+node build.js 
+
 echo "Committing to Git..."
 git add .
-git commit -m "update: sync new posts and assets $(date +'%Y-%m-%d')"
+git commit -m "meh $(date +'%Y-%m-%d')"
 
 echo "Pushing to GitHub..."
 git push origin main
